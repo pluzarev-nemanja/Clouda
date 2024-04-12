2e5cdf53acdfc642e918ef3dccf05fddf0ff2d70
nemanja.pluzarev <pluzarev.nemanja@gmail.com>
2024-04-12 10:10:19 +0200

    AirPollutionScreen.kt and WeeklyWeatherScreen added

---
+++ app/src/main/java/com/example/weatherapp/airPollution/screen/pollution/AirPollutionScreen.kt
@@ -0,0 +1,8 @@
+package com.example.weatherapp.airPollution.screen.pollution
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun AirPollutionScreen() {
+
+}
---
+++ app/src/main/java/com/example/weatherapp/weeklyWeather/screen/weekly/WeeklyWeatherScreen.kt
@@ -0,0 +1,8 @@
+package com.example.weatherapp.weeklyWeather.screen.weekly
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun WeeklyWeatherScreen() {
+
+}
