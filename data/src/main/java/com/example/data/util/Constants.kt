24fa550993c7c71a71dcafbe28da27bbc99dba72
Nemanja Pluzarev <107317386+pluzarev-nemanja@users.noreply.github.com>
2024-10-05 10:36:39 +0200

    Update Constants.kt

--- data/src/main/java/com/example/data/util/Constants.kt
+++ data/src/main/java/com/example/data/util/Constants.kt
@@ -3,7 +3,7 @@ package com.example.data.util
 data object Constants {
 
     const val BASE_URL = "https://api.openweathermap.org/data/2.5/"
-    const val API_KEY = "0f4d191c80961f7a25d8be262f12244b"
+    const val API_KEY = "API KEY"
 
 
 }
